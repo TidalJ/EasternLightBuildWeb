7b44f60177264ef030749d87ed096891314e902c
TidalJ <395478675@qq.com>
2023-02-27 00:20:04 +1030

    change route

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + "/public", { index: "home.html" }));
+app.use(express.static(__dirname, { index: "index.html" }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
